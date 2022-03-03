e554a54d7fc0f892eb159524fd1222ccde97781f
rain <206131521@qq.com>
2022-03-03 16:03:08 +0800

    add bytom

--- native/service/cross_chain_manager/entrance.go
+++ native/service/cross_chain_manager/entrance.go
@@ -19,6 +19,7 @@ package cross_chain_manager
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/polynetwork/poly/native/service/cross_chain_manager/bytom"
 
 	"github.com/polynetwork/poly/native/service/cross_chain_manager/consensus_vote"
 	"github.com/polynetwork/poly/native/service/cross_chain_manager/pixiechain"
@@ -100,6 +101,8 @@ func GetChainHandler(router uint64) (scom.ChainHandler, error) {
 		return pixiechain.NewPixieHandler(), nil
 	case utils.STARCOIN_ROUTER:
 		return starcoin.NewHandler(), nil
+	case utils.BYTOM_ROUTER:
+		return bytom.NewHandler(), nil
 	default:
 		return nil, fmt.Errorf("not a supported router:%d", router)
 	}
